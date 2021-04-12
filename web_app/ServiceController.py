2fdb88249bb1a14e10c17e4fc154abedb017df15
Pierre Kancir <pierre.kancir.emn@gmail.com>
2021-04-12 18:41:31 +0200

    sdfsdf

--- web_app/ServiceController.py
+++ web_app/ServiceController.py
@@ -7,41 +7,41 @@ class ServiceController(object):
         A simple supervisor commands to manage services
     """
 
-    manager = ServerProxy('http://127.0.0.1',
-                          transport=supervisor.xmlrpc.SupervisorTransport(None, None, 'unix:///tmp/supervisor.sock'))
-
     def __init__(self, unit):
         """
             param: unit: a systemd unit name (ie str2str_tcp...)
         """
         self.unit = unit.split(".")[0]
-        
+        self.manager = ServerProxy('http://127.0.0.1',
+                                   transport=supervisor.xmlrpc.SupervisorTransport(None, None,
+                                                                                   'unix:///tmp/supervisor.sock'))
+
     def isActive(self):
-        if manager.supervisor.getProcessInfo(self.unit)["state"] == 20:
+        if self.manager.supervisor.getProcessInfo(self.unit)["state"] == 20:
             return True
-        elif manager.supervisor.getProcessInfo(self.unit)["state"] == 10:
-            #TODO manage this transitionnal state differently
+        elif self.manager.supervisor.getProcessInfo(self.unit)["state"] == 10:
+            # TODO manage this transitionnal state differently
             return True
         else:
             return False
 
     def getUser(self):
         return "root"  # TODO : fix this
-    
+
     def status(self):
-        return manager.supervisor.getProcessInfo(self.unit)["statename"]
+        return self.manager.supervisor.getProcessInfo(self.unit)["statename"]
 
     def status_full(self):
-        return str(manager.supervisor.getProcessInfo(self.unit))
+        return str(self.manager.supervisor.getProcessInfo(self.unit))
 
     def get_log(self):
-        return str(manager.supervisor.tailProcessLog(self.unit), 0, 2000)
+        return str(self.manager.supervisor.tailProcessLog(self.unit), 0, 2000)
 
     def start(self):
-        manager.supervisor.startProcess(self.unit)
+        self.manager.supervisor.startProcess(self.unit)
 
     def stop(self):
-        manager.supervisor.stopProcess(self.unit)
+        self.manager.supervisor.stopProcess(self.unit)
 
     def restart(self):
         self.stop()
